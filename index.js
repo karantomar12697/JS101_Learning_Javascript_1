cfa5a73cba91be2cfc0c01edc0cd4a4f9b566441
KARAN TOMAR <112754511+karantomar12697@users.noreply.github.com>
2022-10-10 21:45:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ let name = "karan";
 console.log(name);
 let father_name = "Rakesh";
 console.log(father_name);
-let mother_name = "Gaytri";
+let mother_name = "Gaytri Devi";
 console.log(mother_name);
